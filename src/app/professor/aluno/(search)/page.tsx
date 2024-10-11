af2b2b0045ed600d8740b336612fac4b6bcbe1a9
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2024-10-11 13:56:56 -0300

    final-TCC

--- src/app/professor/aluno/(search)/page.tsx
+++ src/app/professor/aluno/(search)/page.tsx
@@ -18,7 +18,6 @@ export default function SearchTeacher() {
     return (
         <main className="mt-6">
             <div className="flex gap-4">
-                <Button className="mb-3"><Link href={routes.aluno.new}>Novo aluno</Link></Button>
                 <Button className="mb-3"><Link href={routes.faltasDisciplinares.new}>Nova falta disciplinar</Link></Button>
             </div>
             <Table>
